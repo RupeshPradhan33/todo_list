9287f670ff0405d396d4c8a3b008dfae5f06daca
RupeshPradhan33 <163344520+RupeshPradhan33@users.noreply.github.com>
2026-03-28 23:31:18 +0530

    form adding in progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div className='container'>
+  return (
+    <div className='container'>
+      <form >
+        <input type="text" placeholder='Title' />
+        <textarea placeholder='Description'></textarea>
+        
+        <button type='submit'>ADD</button>
 
-  </div>
+
+      </form>
+
+    </div>
+  );
 };
 
 export default Home;
