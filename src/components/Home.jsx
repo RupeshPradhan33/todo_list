80487926ad66ca5c5ec1b2f45fd6b95712ffaca5
RupeshPradhan33 <163344520+RupeshPradhan33@users.noreply.github.com>
2026-03-30 17:48:53 +0530

    Form part is in process

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 
+import Task from './Task';
+
 const Home = () => {
   return (
     <div className='container'>
+      <h1>DAILY GOALS</h1>
       <form >
         <input type="text" placeholder='Title' />
         <textarea placeholder='Description'></textarea>
-        
+          
         <button type='submit'>ADD</button>
 
 
       </form>
 
+      <Task />
     </div>
   );
 };
