0893beb9e246b8d0c32619326ce32dc5e58d535a
RupeshPradhan33 <163344520+RupeshPradhan33@users.noreply.github.com>
2026-04-02 23:17:28 +0530

    Half work of Task adding is done.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,48 @@ import React, { useState } from 'react';
 import Task from './Task';
 
 const Home = () => {
+
+  const [tasks, setTasks] = useState([]);  // State to hold the list of tasks(using like array)
+  const [title, setTitle] = useState('');  // State to hold the title of a new task
+  const [description, setDescription] = useState('');  // State to hold the description of a new task
+
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setTasks([...tasks, { title, description }]);  // Add a new task to the list of tasks (using like array)
+
+  };
+
   return (
     <div className='container'>
       <h1>DAILY GOALS</h1>
-      <form >
-        <input type="text" placeholder='Title' />
-        <textarea placeholder='Description'></textarea>
-          
+      <form onSubmit={submitHandler}>
+
+        <input
+          type="text"
+          placeholder='Title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <textarea
+          placeholder='Description'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}>
+
+        </textarea>
+
         <button type='submit'>ADD</button>
 
 
       </form>
 
-      <Task />
+      {tasks.map((item, index) => (
+        <Task key={index} title={item.title} description={item.description} />
+      ))}
     </div>
   );
 };
