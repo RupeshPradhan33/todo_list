0f0718daf23aee0277261181f236ff90124b9751
RupeshPradhan33 <163344520+RupeshPradhan33@users.noreply.github.com>
2026-04-07 17:17:26 +0530

    Home,Contact and about is in process

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Home />
+      </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,17 @@
 
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
 const Header = () => {
-  return (
+  return <>
     <nav>Get ready to complete your Daily Goals</nav>
-  )
-}
+    <div className="header">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+  </>
+};
 export default Header;
+
